5a1f34126265e05fc3782d5fc798b03e6b216ffe
Volyarosh94 <vol.yarosh.94@gmail.com>
2025-02-24 15:31:01 +0200

    Made more changes

--- src/sections/Approach.jsx
+++ src/sections/Approach.jsx
@@ -6,7 +6,7 @@ const Approach = () => {
             <div className="mb-4 font-normal text-[#282B26] lg:w-[700px] lg:text-md text-md font-firaSansRegular leading-normal md:w-[650px]">
                 <p>I collaborate with cross-functional teams through continuous discovery to build a deep understanding of the problem space, so we can deliver product outcomes that are viable, valuable, feasible, usable and ethical.</p>
             </div>
-            <div className="flex flex-col lg:text-left lg:flex lg:justify-start lg:w-[300px] lg:h-[250px] mt-[50px] md:w-[300px] md:h-[250px] sm:w-[250px] sm:h-[200px] max-sm:w-[200px] max-sm:h-[260px] max-sm:w-[320px]">
+            <div className="flex flex-col lg:text-left lg:flex lg:justify-start lg:w-[400px] lg:h-[340px] mt-[50px] md:w-[300px] md:h-[250px] sm:w-[250px] sm:h-[200px] max-sm:w-[200px] max-sm:h-[260px] max-sm:w-[320px]">
                 <img src={discovery} className='w-full h-full' alt="A diagram showing the product development process that Charles takes in his work. At the centre is 'HMW' (How Might We) with four key stages arranged in a diamond shape: Discover, Define, Develop, and Deliver. The left side represents the Problem Space, containing Discover and Define stages, while the right side shows the Solution Space with Develop and Deliver stages. The process begins with (OPPORTUNITY) and ends with (OUTCOME). An overarching arc labeled (CONTINUOUS DISCOVERY) joins OUTCOMES to new OPPORTUNITIES. Curved arrows indicate the flow between stages, and the importance of revisiting opportunities and the HMW when new insight is discovered. The diagram emphasises an iterative, collaborative approach." />
             </div>
         </div>
